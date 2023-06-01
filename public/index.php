36332aea5371fa1a8d72fa06cbe93d2e660391a7
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-06-01 11:22:17 +0330

    create router

--- public/index.php
+++ public/index.php
@@ -4,3 +4,11 @@
 // Require composer autoloader
 require __DIR__ . '/../vendor/autoload.php';
 
+// Create Router instance
+$router = new \Bramus\Router\Router();
+
+$router->get('/article/(\w+)', function () {
+
+});
+
+$router->run();
