0a607bc1bcfcf3f9701ec5e8aa0ee7193fdc68db
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-06-01 11:30:37 +0330

    create Request class

--- public/index.php
+++ public/index.php
@@ -6,16 +6,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Create Router instance
 $router = new \Bramus\Router\Router();
-$request = new \Symfony\Component\HttpFoundation\Request(
-    $_GET,
-    $_POST,
-    [],
-    $_COOKIE,
-    $_FILES,
-    $_SERVER
-);
 
-$router->get('/article/(\w+)', function () use($request) {
+$router->get('/article/(\w+)', function () {
 
 });
 
