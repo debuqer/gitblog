ae33dce9c6c94b5c737b4016a4ac6f099fa1a487
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-06-03 16:30:55 +0330

    load .env

--- public/index.php
+++ public/index.php
@@ -6,6 +6,8 @@ use Debuqer\Kati\Http\Request;
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../helpers/app.php';
 
+$dotenv = Dotenv\Dotenv::createMutable(root_path());
+$dotenv->load();
 
 $kernel = new Http();
 
