ef15de8f9bc5c1413194c7b170810eebce1cefd0
Mohammad Abbasi <m.abbasi@dadehpardaz.com>
2023-06-06 14:47:05 +0330

    some hcnage in tempalte

--- templates/index.php
+++ templates/index.php
@@ -1,7 +1,15 @@
 <?php $this->layout('layout::base', ['title' => $title]) ?>
 
 <?php $this->start('content_') ?>
-
-    <?= \Michelf\Markdown::defaultTransform($readme) ?>
+    <div class="container">
+    <!-- begin post -->
+        <div class="card">
+            <a href="<?= $user->profile ?>"></a>
+            <div class="card-block">
+                <?= \Michelf\Markdown::defaultTransform($readme) ?>
+            </div>
+        </div>
+    <!-- end post -->
+    </div>
 
 <?php $this->stop() ?>
